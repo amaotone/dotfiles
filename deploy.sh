8dbbb2f5a05e7836ba8aab7f2b0c22f830d65000
Amane Suzuki <amane.suzu@gmail.com>
2017-02-06 00:18:55 +0900

    Simplify

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 set -e
 DOT_DIRECTORY="${HOME}/dotfiles"
-DOT_TARBALL="https://github.com/amaotone/dotfiles/tarball/master"
-REMOTE_URL="git@github.com:amaotone/dotfiles.git"
-
-has() {
-  type "$1" > /dev/null 2>&1
-}
 
 usage() {
   name=`basename $0`
   cat <<EOF
 Usage:
-  $name [command] [option]
-
-Commands:
-  deploy
+  $name [option]
 
 Options:
   -f $(tput setaf 1)** warning **$(tput sgr0) Overwrite dotfiles.
@@ -24,61 +15,26 @@ EOF
   exit 1
 }
 
-while getopts :f:h opt; do
-  case ${opt} in
-    f)
-      OVERWRITE=true
-      ;;
-    h)
-      usage
-      ;;
+OPTIND=1
+while getopts fh OPT
+do
+  case ${OPT} in
+    f) OVERWRITE=true ;;
+    h) usage ;;
   esac
 done
-shift $((OPTIND - 1))
+shift $(($OPTIND - 1))
 
-# If missing, download and extract the dotfiles repository
-if [ ! -d ${DOT_DIRECTORY} ]; then
-  echo "Downloading dotfiles..."
-  mkdir ${DOT_DIRECTORY}
+for f in .??*
+do
+  [ -n "${OVERWRITE}" -a -e ${HOME}/${f} ] && rm -f ${HOME}/${f}
+  if [ ! -e ${HOME}/${f} ]; then
+    # ignore files
+    [[ ${f} = ".git" ]] && continue
+    [[ ${f} = ".gitignore" ]] && continue
 
-  if has "git"; then
-    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
-  else
-    curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
-    rm -f ${HOME}/dotfiles.tar.gz
+    ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
   fi
+done
 
-  echo $(tput setaf 2)Download dotfiles complete! ✔︎$(tput sgr0)
-fi
-
-link_files() {
-  for f in .??*
-  do
-    # Force remove the vim directory if it's already there
-    [ -n "${OVERWRITE}" -a -e ${HOME}/${f} ] && rm -f ${HOME}/${f}
-    if [ ! -e ${HOME}/${f} ]; then
-      # If you have ignore files, add file/directory name here
-      [[ ${f} = ".git" ]] && continue
-      [[ ${f} = ".gitignore" ]] && continue
-      ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
-    fi
-  done
-
-  echo $(tput setaf 2)Deploy dotfiles complete! ✔︎$(tput sgr0)
-}
-
-command=$1
-[ $# -gt 0 ] && shift
-
-case $command in
-  deploy)
-    link_files
-    ;;
-  *)
-    usage
-    ;;
-esac
-
-exit 0
-
+echo $(tput setaf 2)✔ Dotfiles are deployed.$(tput sgr0)
